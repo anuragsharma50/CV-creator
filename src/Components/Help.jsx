61ac945a3458d29887c8e5b73902e689ed157858
anuragsharma50 <anuragsh868@gmail.com>
2020-07-21 09:45:57 +0530

    modiji

--- src/Components/Help.jsx
+++ src/Components/Help.jsx
@@ -2,19 +2,26 @@ import React from 'react'
 import {Card,Typography} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
+import modiji from '../aatmanirbhar.jpg';
+
 const useStyles = makeStyles((theme) => ({
     root: {
         minWidth: 275,
         width: 1200,
         margin: 'auto',
-        marginTop: theme.spacing(7), 
-        marginBottom: theme.spacing(10),
+        marginTop: theme.spacing(3), 
+        marginBottom: theme.spacing(1.5),
         fontStyle: 'bold',
     },
     heading: {
-        marginTop: 20,
+        marginTop: 10,
         
     },
+    img: {
+        marginTop: theme.spacing(3),
+        height: theme.spacing(50),
+        width: theme.spacing(65)
+    }
 }))
 
 function Help() {
@@ -23,7 +30,9 @@ function Help() {
 
     return (
         <div>
+            <img src={modiji} alt="Aatmnirbhar Bano" className={classes.img}/>
             <Card className={classes.root}>
+                
                 <Typography variant="h2" gutterBottom className={classes.heading}>
                     Aatmnirbhar Bano
                 </Typography>
